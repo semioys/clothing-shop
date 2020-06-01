4aa88d5978bc6740aed7d7d8811a358c4e93eee9
semioys <semioys@gmail.com>
2020-06-01 22:42:55 +0300

    Homepage was added, app component was moved to folder

--- src/components/app/app.component.jsx
+++ src/components/app/app.component.jsx
@@ -1,10 +1,11 @@
 import React from 'react';
+import HomePage from '../../pages/homepage/homepage.component';
 import './app.styles.scss';
 
 function App() {
   return (
     <div>
-      <h1>Hello world!</h1>
+      <HomePage />
     </div>
   );
 }
